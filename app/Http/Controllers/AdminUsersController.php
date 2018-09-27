534073f48a89a1dd572b26ac89b8bfe34dd50e9c
The Goodmans <keola.goodman@gmail.com>
2018-09-27 16:04:42 -0600

    users page working without photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Requests\UsersRequest;
 use App\User;
+use App\Role;
 
 class AdminUsersController extends Controller
 {
@@ -29,7 +32,9 @@ class AdminUsersController extends Controller
     public function create()
     {
         
-        return view('admin.users.create');
+        $roles = Role::pluck('name', 'id')->all();
+
+        return view('admin.users.create', compact('roles'));
 
     }
 
@@ -39,9 +44,16 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        //
+        
+        User::create($request->all());
+
+        return redirect('/admin/users');
+
+        //return $request->all();
+
+
     }
 
     /**
--- app/User.php
+++ app/User.php
@@ -16,7 +16,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email', 
+        'password',
+        'role_id',
+        'is_active',
+        'photo_id',
     ];
 
     /**
